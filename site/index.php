4aa4e1c366078d4f5b69b99f956fb459dbf32f5c
Juliandroid98 <julianpasker@gmail.com>
2019-09-23 15:33:21 +0200

    Logo geüpload

--- site/index.php
+++ site/index.php
@@ -8,7 +8,7 @@
     <meta name="description" content="gezondheidsmeter">
     <meta name="authors" content="Joel, Julian, Jeremy, Miquel, Tiffany & Rens">
     <meta name="keywords" content="gezondheid, meter, gezondheidsmeter, gezond leven, eten, slaap, drugs, drinken">
-    <link rel="icon" href="assets/images/gezondheidsmeter.jpg" type="image/x-icon">
+    <link rel="icon" href="assets/images/logo.png" type="image/x-icon">
     <link rel="stylesheet" href="assets/css/index.css">
     <script type="application/javascript" src="assets/javascript/index.js"></script>
     <title>Gezondheidsmeter - Home</title>
