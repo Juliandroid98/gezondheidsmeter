9fb328073075c68515ec07459ceb260edeb929c6
Juliandroid98 <julianpasker@gmail.com>
2019-09-23 14:53:44 +0200

    Init voor index.php + mappenstructuur geupdated

---
+++ site/index.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="description" content="gezondheidsmeter">
+    <meta name="authors" content="Joel, Julian, Jeremy, Miquel, Tiffany & Rens">
+    <meta name="keywords" content="gezondheid, meter, gezondheidsmeter, gezond leven, eten, slaap, drugs, drinken">
+    <link rel="icon" href="assets/images/gezondheidsmeter.jpg" type="image/x-icon">
+    <link rel="stylesheet" href="assets/css/index.css">
+    <script type="application/javascript" src="assets/javascript/index.js"></script>
+    <title>Gezondheidsmeter - Home</title>
+</head>
+<body>
+    Hello world.
+</body>
+</html>
