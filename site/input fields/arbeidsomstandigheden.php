3b7b08ed42d65561091087a979b2bf4534930827
rens <uixi02@gmail.com>
2019-09-26 15:41:35 +0200

    button group
    button group

--- site/input fields/arbeidsomstandigheden.php
+++ site/input fields/arbeidsomstandigheden.php
@@ -3,24 +3,43 @@
 <head>
     <meta charset="UTF-8">
     <link rel="stylesheet" type="text/css" href="arbeidsomstandigheden.css">
+    <link rel="stylesheet" type="text/css" href="../assets/css/index.css">
+
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
 </head>
 <body>
     <div class="container">
-        geef je werkplek een cijfer
-        <div class="buttongroup-rating">
-            <button class="button-rating-1">1</button>
-            <button class="button-rating-2">2</button>
-            <button class="button-rating-3">3</button>
-            <button class="button-rating-4">4</button>
-            <button class="button-rating-5">5</button>
-            <button class="button-rating-6">6</button>
-            <button class="button-rating-7">7</button>
-            <button class="button-rating-8">8</button>
-            <button class="button-rating-9">9</button>
-            <button class="button-rating-10">10</button>
+        <div class="section1">
+            <div class="buttongroup-rating">
+                <h4 class="text">Geef je werkplek een beoordeling</h4>
+                <button class="button-rating-1">1</button>
+                <button class="button-rating-2">2</button>
+                <button class="button-rating-3">3</button>
+                <button class="button-rating-4">4</button>
+                <button class="button-rating-5">5</button>
+                <button class="button-rating-6">6</button>
+                <button class="button-rating-7">7</button>
+                <button class="button-rating-8">8</button>
+                <button class="button-rating-9">9</button>
+                <button class="button-rating-10">10</button>
+            </div>
+        </div>
+        <div class="section2">
+            <div class="buttongroup-rating">
+                <h4 class="text">Geef je werkdruk een beoordeling</h4>
+                <button class="button-rating-1">1</button>
+                <button class="button-rating-2">2</button>
+                <button class="button-rating-3">3</button>
+                <button class="button-rating-4">4</button>
+                <button class="button-rating-5">5</button>
+                <button class="button-rating-6">6</button>
+                <button class="button-rating-7">7</button>
+                <button class="button-rating-8">8</button>
+                <button class="button-rating-9">9</button>
+                <button class="button-rating-10">10</button>
+            </div>
         </div>
     </div>
 </body>
