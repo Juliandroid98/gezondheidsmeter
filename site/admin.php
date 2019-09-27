47935e23cee226ab4e773072ed207e1c59ddeaa8
joel <jvaneeveren@gmail.com>
2019-09-27 11:56:08 +0200

    activerings kolom toegevoegd bij gebruiker

---
+++ site/admin.php
@@ -0,0 +1,11 @@
+<?php
+	echo "welkom op de admin pagina";
+?>
+<br>
+test
+<br>
+<h6>
+De admin kan iemand admin maken.<br>
+De admin kan iemand geen admin maken.<br>
+De admin kan eten en drinken wijzigen, verwijderen en toevoegen.<br>
+<h6>
