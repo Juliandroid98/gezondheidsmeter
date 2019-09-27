7d0fa0942a97912bd4cecbb39550703a33167ac4
TiffanyG <tgummarus@hotmail.com>
2019-09-27 11:09:33 +0200

    Icoons + Kleur

--- site/vragenformulier.php
+++ site/vragenformulier.php
@@ -83,10 +83,10 @@
         </form>
         <!-- bottom buttons-->
         <div class="bottomcontainer">
-            <div class="bottombuttongroup">
-                <a class="bottombutton" href="dashboard.php"><img class="bottomimg" src="assets/images/dashboard.png" alt="dashboard"></a>
-                <a class="bottombutton  active" href="vragenformulier.php"><img class="bottomimg" src="assets/images/vragen.png" alt="vragenformulier"></a>
-                <a class="bottombutton" href="meldingen.php"><img class="bottomimg" src="assets/images/meldingen.png" alt="meldingen"></a>
+        <div class="bottombuttongroup">
+                <a class="bottombutton_active" href="#"><img class="bottomimg" src="assets/images/dashboard.png" alt="dashboard"></a>
+                <a class="bottombutton" href="vragenformulier.php"><img class="bottomimg" src="assets/images/questions.png" alt="vragenformulier"></a>
+                <a class="bottombutton" href="meldingen.php"><img class="bottomimg" src="assets/images/notifications.png" alt="meldingen"></a>
             </div>
         </div>
     </div>
