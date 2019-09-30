e0ddbcdab5e9e45119ec6d28b178b4a2477ffbfb
Juliandroid98 <julianpasker@gmail.com>
2019-09-30 14:41:27 +0200

    Activatiepagina gemaakt

--- site/account_activeren.php
+++ site/account_activeren.php
@@ -1,1 +1,39 @@
-<?php
+<?php ?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="description" content="gezondheidsmeter">
+    <meta name="authors" content="Joel, Julian, Jeremy, Miquel, Tiffany & Rens">
+    <meta name="keywords" content="gezondheid, meter, gezondheidsmeter, gezond leven, eten, slaap, drugs, drinken">
+    <link rel="icon" href="assets/images/logo.png" type="image/x-icon">
+    <link rel="stylesheet" href="assets/css/index.css">
+    <link rel="stylesheet" href="assets/css/account_activeren.css">
+    <!-- Jquery -->
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
+    <script type="application/javascript" src="assets/javascript/index.js"></script>
+    <title>Gezondheidsmeter - Succesvol</title>
+</head>
+<body>
+<div class="sitecontainer">
+    <!-- header -->
+    <div class="headercontainer">
+        <image class="logosmall" src="assets/images/logo.png" alt="logo">
+            <h1>Activatie</h1>
+            <a style="visibility:hidden;" href="settings.php">
+                <image class="settingsmenu" src="assets/images/settings.png" alt="settings">
+            </a>
+    </div>
+    <!-- content -->
+    <div class="center">
+        Klik op deze knop om uw account te activeren.<br>
+        <a class="button" href="index.php">
+            Account activeren
+        </a>
+    </div>
+</div>
+</body>
+</html>
