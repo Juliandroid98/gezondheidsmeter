34a8c9d864ee9ee983268359088e6d14a9feef78
joel <jvaneeveren@gmail.com>
2019-10-02 09:13:31 +0200

    admin gerepareerd

--- site/assets/javascript/admin.js
+++ site/assets/javascript/admin.js
@@ -1,3 +1,8 @@
 function addRowForInput(){
-
+	if(document.getElementById('addRowGebruiker').style.display=='none'){
+		document.getElementById('addRowGebruiker').style.display='contents';
+	}
+	else{
+		document.getElementById('addRowGebruiker').style.display='none';
+	}
 }
