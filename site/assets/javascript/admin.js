8554709beb6ec0a6f1bc4e01dc78afe48831b167
joel <jvaneeveren@gmail.com>
2019-10-08 12:32:36 +0200

    wijzigen en verwijderen werkt

--- site/assets/javascript/admin.js
+++ site/assets/javascript/admin.js
@@ -6,3 +6,22 @@ function addRowForInput(){
 		document.getElementById('addRowGebruiker').style.display='none';
 	}
 }
+
+function addRowForEdit(ID){
+	if(document.getElementById('addRowEdit'+ID).style.display=='none'){
+		document.getElementById('addRowEdit'+ID).style.display='contents';
+	}
+	else{
+		document.getElementById('addRowEdit'+ID).style.display='none';
+	}
+}
+
+/*function bevestigDelete(){
+	var a = confirm('wilt u verwijderen?');
+	if(a == true){
+		alert('verwijderd');
+	}
+	else{
+		alert('nog niet verwijderd');
+	}
+}*/
