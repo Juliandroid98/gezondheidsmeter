e19c22f84e3a9e014fe43d38ab020152930ef469
rens <uixi02@gmail.com>
2019-10-27 22:00:53 +0100

    chartjs
    kleine aanpassing

--- site/assets/javascript/chartjs.js
+++ site/assets/javascript/chartjs.js
@@ -1,6 +1,6 @@
 function chart(chartSoort) {
 
-    //get range of first and last day of the week
+    var result = [0];
     var today = new Date;
     var DayToday = today.getDate();
     var first = today.getDate() - today.getDay() + 1;
@@ -12,6 +12,26 @@ function chart(chartSoort) {
     var firstDay = new Date(today.setDate(first)).toISOString().slice(0, 10);
     var lastDay = new Date(today.setDate(last)).toISOString().slice(0, 10);
     var now = new Date(today.setDate(dateNow)).toISOString().slice(0, 10);
+    switch (chartSoort) {
+        case 'arbeid':
+            collapseChart = document.getElementById('chartArbeid').getContext('2d');
+            break;
+        case 'drugs':
+            collapseChart = document.getElementById('chartDrugs').getContext('2d');
+            break;
+        case 'slaap':
+            collapseChart = document.getElementById('chartSlaap').getContext('2d');
+            break;
+        case 'drinken':
+            collapseChart = document.getElementById('chartDrinken').getContext('2d');
+            break;
+        case 'eten':
+            collapseChart = document.getElementById('chartEten').getContext('2d');
+            break;
+        case 'sport':
+            collapseChart = document.getElementById('chartSport').getContext('2d');
+            break;
+    }
     $.ajax(
         {
             type: "POST",
@@ -20,39 +40,25 @@ function chart(chartSoort) {
             dataType: 'json',
             success: function (data) {
                 var collapseChart;
-                var result = [];
-                console.log(data);
-                console.log("Lol");
-                var dataLength;
-                if(data.length === null){
-                    dataLength = 1;
-                } else{
-                    dataLength = data.length;
-                }
+                console.log(result);
                 for (i = 0; i < dataLength; i++) {
                     switch (chartSoort) {
                         case 'arbeid':
-                            collapseChart = document.getElementById('chartArbeid').getContext('2d');
                             result.push(arbeid(data[i][0], data[i][1]));
                             break;
                         case 'drugs':
-                            collapseChart = document.getElementById('chartDrugs').getContext('2d');
                             result.push(drugs(data[i][0]));
                             break;
                         case 'slaap':
-                            collapseChart = document.getElementById('chartSlaap').getContext('2d');
                             result.push(slaap(data[i][0], data[i][1]));
                             break;
                         case 'drinken':
-                            collapseChart = document.getElementById('chartDrinken').getContext('2d');
                             result.push(drinken(data[i][0], data[i][1], data[i][2]));
                             break;
                         case 'eten':
-                            collapseChart = document.getElementById('chartEten').getContext('2d');
                             result.push(eten(data[i][0], data[i][1]));
                             break;
                         case 'sport':
-                            collapseChart = document.getElementById('chartSport').getContext('2d');
                             result.push(sporten(data[i][0]));
                             break;
                         case 'alles':
@@ -80,55 +86,56 @@ function chart(chartSoort) {
                 console.log(result);
 
 
-                var chart = new Chart(collapseChart, {
-                    type: 'line',
 
-                    data: {
-                        labels: ['Ma', 'Di', 'Wo', 'Do', 'Vr', 'Za', 'Zo'],
-                        datasets: [{
-                            label: 'Levensstijl',
-                            data: result
-                        }]
-                    },
-                    options: {
-                        responsive: true,
-                        maintainAspectRatio: false,
-                        responsiveAnimationDuration: 1000,
-                        scales: {
-                            yAxes: [{
-                                ticks: {
-                                    suggestedMin: 0,
-                                    beginAtZero: true,
-                                    max: 10
-                                }
-                            }]
-                        }
-                    },
-                    plugins: [
-                        {
-                            afterLayout: function (chart) {
-                                var scales = chart.scales;
-
-                                var borderColor = chart.ctx.createLinearGradient(
-                                    scales["x-axis-0"].left,
-                                    scales["y-axis-0"].bottom,
-                                    scales["y-axis-0"].right,
-                                    scales["y-axis-0"].top
-                                );
-
-                                borderColor.addColorStop(0, "red");
-                                borderColor.addColorStop(0.25, "orange");
-                                borderColor.addColorStop(0.5, "yellow");
-                                borderColor.addColorStop(0.75, "lime");
-                                borderColor.addColorStop(1, "green");
-
-                                chart.data.datasets[0].borderColor = borderColor;
-                            }
-                        }
-                    ]
-
-
-                });
             }
         });
+    var chart = new Chart(collapseChart, {
+        type: 'line',
+
+        data: {
+            labels: ['Ma', 'Di', 'Wo', 'Do', 'Vr', 'Za', 'Zo'],
+            datasets: [{
+                label: 'Levensstijl',
+                data: result
+            }]
+        },
+        options: {
+            responsive: true,
+            maintainAspectRatio: false,
+            responsiveAnimationDuration: 1000,
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        suggestedMin: 0,
+                        beginAtZero: true,
+                        max: 10
+                    }
+                }]
+            }
+        },
+        plugins: [
+            {
+                afterLayout: function (chart) {
+                    var scales = chart.scales;
+
+                    var borderColor = chart.ctx.createLinearGradient(
+                        scales["x-axis-0"].left,
+                        scales["y-axis-0"].bottom,
+                        scales["y-axis-0"].right,
+                        scales["y-axis-0"].top
+                    );
+
+                    borderColor.addColorStop(0, "red");
+                    borderColor.addColorStop(0.25, "orange");
+                    borderColor.addColorStop(0.5, "yellow");
+                    borderColor.addColorStop(0.75, "lime");
+                    borderColor.addColorStop(1, "green");
+
+                    chart.data.datasets[0].borderColor = borderColor;
+                }
+            }
+        ]
+
+
+    });
 }
